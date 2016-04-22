8c15fa7d5807e58a4367a0e8a0d72d34e44c359a
Crispus Mwaura <Cmmwaura@ucdavis.edu>
2016-04-22 11:32:40 -0700

    Update old_settings.py

--- analyst/old_settings.py
+++ analyst/old_settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g2*p$_eijfl-o3b-d@1&c!gxy8@*tfqjrm9wdtp5n^_uefx8z4'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
